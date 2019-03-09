0083245cdf32c6a239badb38596a90c78a555fb3
Bart <Bengelsoft@users.noreply.github.com>
2019-03-09 10:07:27 +0100

    Init commit for pipelines (#6)

---
+++ backend/src/main/java/nl/bengelsoft/scrumpo/ScrumpoApplication.java
@@ -0,0 +1,13 @@
+package nl.bengelsoft.scrumpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumpoApplication.class, args);
+	}
+
+}
